3a4193a848a1c3f8665ca3697ca726c0eb6a0085
leighannemarais <leighannemarais@gmail.com>
2023-05-22 12:30:06 -0700

    Showing the current temperature

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let apiKey = "09bd59ceb39cf5662a213f3b54702d64";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(displayTemperature);
